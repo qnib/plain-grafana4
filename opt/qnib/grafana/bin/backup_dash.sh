3883509d3e8d0d9ec4b6791ecdd5a416f8320cf7
Christian Kniep <christian@qnib.org>
2017-03-31 14:16:02 +0200

    fix GF_PLUGIN_DIR

--- opt/qnib/grafana/bin/backup_dash.sh
+++ opt/qnib/grafana/bin/backup_dash.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-ITEMS=$1
-if [[ -z ${ITEMS} ]];then
-   ITEMS=$(sqlite3 /var/lib/grafana/grafana.db .dump |egrep -o "INSERT INTO \"dashboard\" VALUES\(.*',X" |awk -F, '{print $3}' |tr -d \ |xargs | sed -e 's/ /,/g')
-fi
-for item in $(echo ${ITEMS} |sed -e 's/,/ /g');do
+BACKUP_DIR=$1
+
+for item in $(sqlite3 /var/lib/grafana/grafana.db .dump |egrep -o "INSERT INTO \"dashboard\" VALUES\(.*',X" |awk -F, '{print $3}' |tr -d \ |xargs);do
     if [[ -n ${BACKUP_DIR} ]];then
         sqlite3 /var/lib/grafana/grafana.db .dump | egrep "INSERT INTO \"dashboard\".*${item}" > ${BACKUP_DIR}/${item}.sql
     else
-        sqlite3 /var/lib/grafana/grafana.db .dump | egrep "INSERT INTO \"dashboard\".*${item}" 
+        sqlite3 /var/lib/grafana/grafana.db .dump | egrep "INSERT INTO \"dashboard\".*${item}"
     fi
 done
