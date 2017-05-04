2ea61077e45150d753daaa2ad65240e6544b926c
Christian Kniep <christian@qnib.org>
2017-05-04 22:12:49 +0200

    generic, configurable influxdb data-source

--- opt/qnib/grafana/sql/data-sources/qframe.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO "data_source" VALUES(5,1,0,'influxdb','qframe','proxy','http://INFLUXDB_HOST:8086','root','root','qframe',0,'','',0,X'7B7D','2016-05-16 15:46:19','2017-05-04 20:01:20',0);
