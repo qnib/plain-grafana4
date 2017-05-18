495e7ad48b375591509140bebb8291f1153c67b2
Christian Kniep <christian@qnib.org>
2017-05-18 14:51:13 +0200

    added some plugins, allow for only dashboard-mount

---
+++ opt/qnib/grafana/sql/data-sources/04-qcollect.sql
@@ -0,0 +1,6 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+
+INSERT INTO "data_source" VALUES(4,1,0,'influxdb','qcollect','proxy','http://influxdb:8086','root','root','qcollect',0,'','',0,X'7B7D','2016-05-16 15:46:19','2016-09-13 23:23:37',0);
+
+COMMIT;
