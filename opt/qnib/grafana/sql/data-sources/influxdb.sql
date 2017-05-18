593df49ff9d54eab5689bc8a29c96bcc04ce3fef
Christian Kniep <christian@qnib.org>
2017-05-18 15:14:57 +0200

    bugfix bootstrap

--- opt/qnib/grafana/sql/data-sources/influxdb.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO "data_source" VALUES(5,1,0,'influxdb','INFLUXDB_DB','proxy','http://INFLUXDB_HOST:8086','root','root','INFLUXDB_DB',0,'','',0,X'7B7D','2016-05-16 15:46:19','2017-05-04 20:01:20',0);
