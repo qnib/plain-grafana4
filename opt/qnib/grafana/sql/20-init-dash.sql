e822ded2813bc69ccd5e1e10e7118f6e0821433a
Christian Kniep <christian@qnib.org>
2017-12-08 11:45:20 +0100

    add dashboard_version and _snapshot table

--- opt/qnib/grafana/sql/20-init-dash.sql
+++ opt/qnib/grafana/sql/20-init-dash.sql
@@ -15,5 +15,29 @@ CREATE TABLE `dashboard` (
 , `created` DATETIME NOT NULL
 , `updated` DATETIME NOT NULL
 , `updated_by` INTEGER NULL, `created_by` INTEGER NULL, `gnet_id` INTEGER NULL, `plugin_id` TEXT NULL);
-
+CREATE TABLE `dashboard_version` (
+`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
+, `dashboard_id` INTEGER NOT NULL
+, `parent_version` INTEGER NOT NULL
+, `restored_from` INTEGER NOT NULL
+, `version` INTEGER NOT NULL
+, `created` DATETIME NOT NULL
+, `created_by` INTEGER NOT NULL
+, `message` TEXT NOT NULL
+, `data` TEXT NOT NULL
+);
+CREATE TABLE `dashboard_snapshot` (
+`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL
+, `name` TEXT NOT NULL
+, `key` TEXT NOT NULL
+, `delete_key` TEXT NOT NULL
+, `org_id` INTEGER NOT NULL
+, `user_id` INTEGER NOT NULL
+, `external` INTEGER NOT NULL
+, `external_url` TEXT NOT NULL
+, `dashboard` TEXT NOT NULL
+, `expires` DATETIME NOT NULL
+, `created` DATETIME NOT NULL
+, `updated` DATETIME NOT NULL
+);
 COMMIT;
